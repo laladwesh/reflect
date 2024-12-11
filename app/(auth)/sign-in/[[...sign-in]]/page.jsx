654f31c55873bb664eeb0ee6f1af9ee1835768c0
Avinash Gupta <guptaavinash302@gmail.com>
2024-12-11 20:51:37 +0530

    vercel deployment tested all things

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,8 @@
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const SignInPage = () => {
+  return <SignIn/>
+}
+
+export default SignInPage
---
+++ app/(main)/journal/[id]/loading.jsx
@@ -0,0 +1,7 @@
+
+import { BarLoader } from "react-spinners";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <BarLoader color="orange" width={"100%"} />;
+}
